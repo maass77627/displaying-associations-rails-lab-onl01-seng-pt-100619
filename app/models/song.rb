7d7fa96a796e7884d9fbedae628078f1f80974e2
kelly_maass <kmaass77627@gmail.com>
2020-04-07 18:38:53 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,12 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  def artist_name
+    self.artist.name 
+  end 
+
+  def artist_song_name
+    artist_name + " - " + self.title 
+  end 
+
 end
